fcf7f211ec9813760a3e93bb3b3918854d8e8c43
fabriciofmsilva <fabriciofmsilva@gmail.com>
2014-12-23 23:56:22 -0200

    Update styles

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,18 +7,45 @@ module.exports = function(grunt) {
       files: {
         expand: true,
         cwd: 'src/',
-        src: ['**/*'],
+        src: ['**/*', '!styles/**'],
         dest: 'dist/'
+      }
+    },
+
+    sass: {
+      dev: {
+        options: {
+          style: 'expanded'
+        },
+        files: {
+          'dist/css/main.css': ['src/styles/main.scss']
+        }
+      }
+    },
+
+    watch: {
+      javacript: {
+        files: ['src/**/*', '!styles/**'],
+        tasks: ['newer:copy']
+      },
+      sass: {
+        files: ['src/**/*'],
+        tasks: ['newer:sass']
       }
     }
   });
 
 
   // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-newer');
   grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+
 
   // Default task(s).
   grunt.registerTask('default', [
+    'sass',
     'copy'
     ]
   );
